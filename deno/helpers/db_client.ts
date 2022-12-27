562bacaacb0c61187e15d0c17397307f9b0f2bc4
Nirmalya Nayak <n.nayak263@gmail.com>
2022-12-27 20:20:10 +0530

    implemented get and post method with mongodb

--- deno/helpers/db_client.ts
+++ deno/helpers/db_client.ts
@@ -1,11 +1,11 @@
-import { MongoClient, Database } from 'https://deno.land/x/mongo@v0.8.0/mod.ts'
+import { MongoClient, Database } from 'https://deno.land/x/mongo/mod.ts'
 
 let db: Database
 
-export function connect() {
+export async function connect() {
   const client = new MongoClient()
-  client.connectWithUri(
-    'mongodb+srv://nirmalya:nirmalya@cluster.a9tjk7u.mongodb.net/',
+  await client.connect(
+    'mongodb+srv://nirmalya:nirmalya@cluster.a9tjk7u.mongodb.net/test-deno-todo?authMechanism=SCRAM-SHA-1',
   )
 
   db = client.database('test-deno-todo')
--- deno/routes/todos.ts
+++ deno/routes/todos.ts
@@ -1,3 +1,4 @@
+import { ObjectId } from 'https://deno.land/x/mongo/mod.ts'
 import { Router } from 'https://deno.land/x/oak/mod.ts'
 
 import { getDB } from '../helpers/db_client.ts'
@@ -5,25 +6,35 @@ import { getDB } from '../helpers/db_client.ts'
 const router = new Router()
 
 interface Todo {
-  id: string
+  id?: string
   text: string
 }
 
 let todos: Todo[] = []
 
-router.get('/todos', (ctx) => {
-  ctx.response.body = { todos: todos }
+router.get('/todos', async (ctx) => {
+  const todos = await getDB().collection('todos').find()
+  const transformedToDos = await todos.map(
+    (todo: { _id: string; text: string }) => {
+      return {
+        id: todo._id.toString(),
+        text: todo.text,
+      }
+    },
+  )
+  ctx.response.body = { todos: transformedToDos }
 })
 
 router.post('/todos', async (ctx) => {
   const data = await ctx.request.body()
   const finalData = await data.value
   const newTodo: Todo = {
-    id: new Date().toISOString(),
+    // id: new Date().toISOString(),
     text: finalData.text,
   }
+  const id = await getDB().collection('todos').insertOne(newTodo)
 
-  todos.push(newTodo)
+  newTodo.id = id.$oid
 
   ctx.response.body = { message: 'Created todo!', todo: newTodo }
 })
